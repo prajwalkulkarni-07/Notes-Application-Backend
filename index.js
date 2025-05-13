6c2f7bb6ddf288457d3334d15f66dd1d05bf6294
Prajwal Kulkarni <mkprajwal07@gmail.com>
2025-05-13 13:07:52 +0530

    Changed origin in index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENTURL || "http://localhost:5173",
+    origin: true, // Reflect request origin
     credentials: true,
   })
 );
